5c322e6dc001a3ca37c10365ce8c60c98420631d
Kim SongHyeon <thiagoo@naver.com>
2026-01-22 13:11:07 +0900

    feat: 링크 공유용 Open Graph 이미지 추가

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -28,11 +28,20 @@ export const metadata: Metadata = {
     type: 'website',
     locale: 'ko_KR',
     siteName: '뽑기중독',
+    images: [
+      {
+        url: '/og-image.png',
+        width: 1200,
+        height: 630,
+        alt: '뽑기중독 게임 화면',
+      },
+    ],
   },
   twitter: {
     card: 'summary_large_image',
     title: '뽑기중독 | 리얼한 웹 인형뽑기 게임',
     description: '리얼한 물리 엔진으로 즐기는 웹 인형뽑기 게임',
+    images: ['/og-image.png'],
   },
 };
 
