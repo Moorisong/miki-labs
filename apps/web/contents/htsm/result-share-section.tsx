3558c4fff083b5ff9d8c28eb0454649182d73559
Moorisong <thiagoo@naver.com>
2026-02-16 15:06:33 +0900

    refactor: 카카오 공유하기 문구 변경

--- apps/web/contents/htsm/result-share-section.tsx
+++ apps/web/contents/htsm/result-share-section.tsx
@@ -53,7 +53,7 @@ export default function ResultShareSection({ shareId }: ResultShareSectionProps)
                 objectType: 'feed',
                 content: {
                     title: '[자아탐험] 친구의 키워드를 선택해주세요',
-                    description: '당신의 선택 하나가 누군가의 ‘숨겨진 자아’를 완성합니다.',
+                    description: '당신의 선택 하나가 누군가의 ‘숨겨진 자아’를 완성합니다. 친구를 가장 잘 표현하는 3가지 키워드를 익명으로 선택해주세요.',
                     imageUrl: 'https://box.haroo.site/htsm-og-base.png',
                     link: { mobileWebUrl: inviteUrl, webUrl: inviteUrl },
                 },
